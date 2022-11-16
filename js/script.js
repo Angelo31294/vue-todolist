8455755f7b03fd0016561e81144ae92fc9bfe162
Angelo31294 <luongoangelo1@gmail.com>
2022-11-16 10:37:39 +0100

    Correzione

--- js/script.js
+++ js/script.js
@@ -2,10 +2,36 @@
 
 const { createApp } = Vue
 
-  createApp({
-    data() {
-      return {
-        message: 'Hello Vue!'
-      }
+createApp({
+  data() {
+    return {
+      newText:'',
+      todos:[
+        {
+          text:"Fare la spesa",
+          done: true,
+        },
+        {
+          text:"Fare i compiti",
+          done: false,
+        },
+        {
+          text:"andare dalla nonna",
+          done: false,
+        }
+      ],
     }
-  }).mount('#app')
+  },
+  methods: {
+    removeTodo(i) {
+      this.todos.splice(i, 1);
+    }
+  },
+  addTodo() {
+    const objNewTodo = {
+      text: this.newText,
+      done: false
+    };
+    this.todos.push(objNewTodo);
+  }
+}).mount('#app')
